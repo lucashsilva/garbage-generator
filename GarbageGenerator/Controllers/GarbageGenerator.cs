80d43c640b50774172ad0d7f07717dd7ca8a004c
Lucas Silva <lucashenrique125@gmail.com>
2020-04-08 21:13:31 -0300

    First commit - garbage generator application with docker

---
+++ GarbageGenerator/Controllers/GarbageGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GarbageGenerator.Controllers
+{
+    [ApiController]
+    [Route("garbage")]
+    public class GarbageGeneratorController : ControllerBase
+    {
+        private readonly ILogger<GarbageGeneratorController> _logger;
+
+        public GarbageGeneratorController(ILogger<GarbageGeneratorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            var size = Math.Pow(2, 20);
+            var list = new List<Double>();
+
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(i);
+            }
+
+            return Ok();
+        }
+    }
+}
